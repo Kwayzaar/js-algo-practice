5c49e456c638cbbd86495284f85e2f6013004a91
eric <lee.eric.03@gmail.com>
2021-08-02 12:59:28 -0600

    adds lessons

--- js-alg-practice.js
+++ js-alg-practice.js
@@ -1,30 +1,52 @@
 //FREE CODE CAMP - DS A&& ALGORITHMS\\
 
 //CARDCOUNTER\\
-var count = 0;
+// var count = 0;
 
-function cc(card) {
-  switch(card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++
-      break;
-    case 10: 
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--
-      break;
-  }
-  if (count > 0) {
-    return count + " Bet"
-  } else {
-    return count +  " Hold"
-  } 
-}
+// function cc(card) {
+//   switch(card) {
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//       count++
+//       break;
+//     case 10: 
+//     case "J":
+//     case "Q":
+//     case "K":
+//     case "A":
+//       count--
+//       break;
+//   }
+//   if (count > 0) {
+//     return count + " Bet"
+//   } else {
+//     return count +  " Hold"
+//   } 
+// }
 
-cc(2); cc(3); cc(7); cc('K'); cc('A')
+// cc(2); cc(3); cc(7); cc('K'); cc('A')
+
+//Dot notation\\
+// var testObj = {
+//   "hat": "ballcap",
+//   "shirt": "jersey",
+//   "shoes": "cleats"
+// };
+// //assign value of hat and shirt
+// var hatValue = testObj.hat;      
+// var shirtValue = testObj.shirt;  
+
+//Bracket notation, should use when spaces in names 
+// var testObj = {
+//   "an entree": "hamburger",
+//   mySide: "veggies",
+//   "the drink": "water"
+// };
+
+// var entreeValue = testObj["an entree"];   
+// //put property in quotations, even if key isn't
+// //keys are automatically typecast as strings 
+// var drinkValue = testObj["the drink"];
