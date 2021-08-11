05e6788dffaba32490c4dd34aae07d87c218c78d
eric <lee.eric.03@gmail.com>
2021-08-11 08:10:32 -0600

    adds lessons

--- js-es6.js
+++ js-es6.js
@@ -13,3 +13,73 @@
 
 // common practice with const variable: use UPPERCASE letters 
 // mutable values (let, var): use camelCase 
+
+// Object.freeze \\
+//prevents data mutation 
+// Object.freeze(obj)
+
+// Arrow Functions \\
+
+// Inline function that allows us to not have to name a function
+// used especially if we are passing that function as an arugment to another funtion 
+// Example
+
+// const myFunc = function() {
+//   const myVar = "value";
+//   return myVar;
+// }
+
+// --turns to--
+
+// const myFunc = () => {
+//   const myVAr = "value"
+//   return myVar
+// }
+
+// -- may also omit return keyword above when only a return value --
+
+// const myFunc = () => "value"
+
+//1 more example
+// var magic = function() {
+//   return new Date();
+// };
+//turns to 
+// const magic = () => new Date()
+
+// Arrow functions with parameters
+// Can pass arguments into arrow function just like regular functions
+// If only 1 parameter, parantheses that enclose may be omitted 
+// Example 
+
+// var myConcat = function(arr1, arr2) {
+//   return arr1.concat(arr2);
+// };
+// to 
+// const myConcat = (arr1, arr2) => arr1.concat(arr2)
+// myConcat([1,2], [3,4,5]) --> [1,2,3,4,5]
+
+//Arrow functions with default parameters 
+// Used when no argument is specified 
+//Ex:
+//add 1 to number when value is not specified 
+//const increment = (number, value = 1) => number + value 
+//increment(2) --> 3
+
+// Rest Parameter \\
+// allows a function to take a variable number of arguments 
+// allows us to appy map(), filter(), and reduce() to parameters 
+//Ex: 
+// const sum = (x, y, z) => {
+//   const args = [x, y, z];
+//   return args.reduce((a, b) => a + b, 0);
+// }
+
+// rest parameter ...args allows us to remove the constant args and just input the arugments we want
+// const sum = (...args) => {
+//   return args.reduce((a,b) => a + b, 0)
+// }
+// sum(1,2,3,4) --> 10
+
+// Spread Operator \\
+
