0e919e3e27a38d910841e1b2ef4c4ad25418b426
eric <lee.eric.03@gmail.com>
2021-09-25 14:21:35 -0600

    add basic DS lessons

--- js-adv-ds-alg.js
+++ js-adv-ds-alg.js
@@ -17,7 +17,9 @@
 // removes first element from array 
 
 // splice() \\
-// removing items
+// splice() mutates the original array 
+
+// removing items with splice()
 // use first two paramters splice(1,2)
 // first param indicates which index in array to start splice at (element here is included in the splice)
 // second param indicates the number of elements to delete 
@@ -27,3 +29,29 @@
 // first two parameters are the same as above 
 // third param indicates what to replace the spliced elements with 
 // third parameter can take as many elements as necessary splice(1,2,3,4,5,6)
+
+// COPYING ARRAYS\\
+
+// slice()
+// copies (extracts) a number of elements from an array, not mutating original array
+// two params slice(1,2)
+// first param is index at which to begin extraction
+// second is index at which to end extraction. item at this index is not included in the slice 
+
+// spread operator [...]
+// allows us to copy an entire array, in order, and does not mutate original array 
+// ex:
+// let thisArray = [true, true, undefined, false, null];
+// let thatArray = [...thisArray];
+// --> thisArray remains same, thatArray = thisArray 
+
+// Combining arrays with spread operator 
+// we can insert an array into another array at any idex using spread syntax
+// ex:d
+// function spreadOut() {
+//   let fragment = ['to', 'code'];
+//   let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+//   return sentence;
+// }
+// --> [ 'learning', 'to', 'code', 'is', 'fun' ]
+
