dd36d5b5d110b71aeafae8756aeab31ae1b18ec0
eric <lee.eric.03@gmail.com>
2021-10-05 12:27:33 -0600

    adds lesson to basic scripting

--- js-basic-algorithm-scripting.js
+++ js-basic-algorithm-scripting.js
@@ -23,3 +23,18 @@
 // factorialize(5);
 // --> 120
 
+// FIND LONGEST WORD IN A STRING 
+
+// function findLongestWordLength(str) {
+//   let split = str.split(" ")
+//   let longestWord = 0 
+//   for (let i = 0; i < split.length; i++) {
+//     if(split[i].length > longestWord) {
+//       longestWord = split[i].length
+//     }
+//   }
+//   return longestWord
+// }
+
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
+//  --> 6 ("jumped")
