4502ad2283374ab14bdc0a28ed23799ddf75b77d
eric <lee.eric.03@gmail.com>
2021-10-06 11:20:48 -0600

    adds lesson to basic sccripting

--- js-basic-algorithm-scripting.js
+++ js-basic-algorithm-scripting.js
@@ -53,3 +53,15 @@
 
 // largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 // --> [ 5, 27, 39, 1001 ]
+
+// CONFIRM THE END OF A STRING
+// function confirmEnding(str, target) {
+//   if(str.substring(str.length - target.length) === target){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// confirmEnding("Bastian", "n")
+//  --> returns true 
