13f0aae4a3e98d4c27258376603d25f0b85ccbab
Eric <lee.eric.03@gmail.com>
2021-10-09 22:15:15 -0600

    adds lesson to basic scripting

--- js-basic-algorithm-scripting.js
+++ js-basic-algorithm-scripting.js
@@ -183,3 +183,15 @@
 
 // mutation(["hello", "hey"])
 // --> false
+
+// CHUNK AN ARRAY BY SIZE
+// function chunkArrayInGroups(arr, size) {
+//   let newArr = []
+//   for(let i = 0; i < arr.length; i+= size){
+//     newArr.push(arr.slice(i,i + size)) 
+//   }
+//   return newArr
+// }
+ 
+// chunkArrayInGroups(["a", "b", "c", "d"], 2)
+// --> returns [[a,b], [c,d]]
