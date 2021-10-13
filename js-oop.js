2dfef7e6e482b82ba55ccfd986c87d374b36ea35
eric <lee.eric.03@gmail.com>
2021-10-13 16:14:25 -0600

    adds lesson to OOP

---
+++ js-oop.js
@@ -0,0 +1,12 @@
+// CONSTRUCTORS \\
+// create new objects and define the properties and behaviors of that object 
+// conventions: 
+  // defined with capitalized name to distinguish from functions
+  // use "this" to set properties of object they will create
+  // define properties and values instead of returning values, like a function would 
+// ex:
+// function Dog() {
+//   this.name = "Remy";
+//   this.color = "Gray";
+//   this.numLegs = 4;
+// }
