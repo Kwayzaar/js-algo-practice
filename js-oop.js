69d67249ee41d3883e680043dd604c81edb3d3f3
eric <lee.eric.03@gmail.com>
2021-10-13 17:09:41 -0600

    adds lessons to OOP

--- js-oop.js
+++ js-oop.js
@@ -11,7 +11,7 @@
 //   this.numLegs = 4;
 // }
 
-// Using a constructor to create Objects \\
+// USING CONSTRUCTORS TO CREATE OBJECTS \\
 // We can create a new object by calling the constructor with "new", and assign it a name 
 // The constructor will give the object all the properties it has 
 // function Dog() {
@@ -22,4 +22,15 @@
 
 // let hound = new Dog() //this is the object being created 
 // --> hound = { name: 'Rupert', color: 'brown', numLegs: 4 }
+
+// EXTEND CONSTRUCTOR TO RECEIVE ARGUMENTS \\
+// We can create constructors to accept parameters, so that we can pass new values into objects when they are created with a constructor 
+// function Dog(name, color) {
+//   this.name = name // <-- pass the arguments in here 
+//   this.color = color //<-- and here 
+//   this.numLegs = 4
+// }
+
+// let terrier = new Dog("Gus", "white")
+// --> { name: 'Gus', color: 'white', numLegs: 4 }
 
